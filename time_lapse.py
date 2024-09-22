ee37e254b2de173e3185cd03a8270879e54549a3
jtomfarrar <jfarrar@whoi.edu>
2024-09-21 20:53:22 -0400

    update

--- time_lapse.py
+++ time_lapse.py
@@ -14,13 +14,13 @@ picam2 = Picamera2()
 camera_config = picam2.create_preview_configuration()
 picam2.configure(camera_config)
 
-output_dir_prefix = "../img/time_lapse/"
+output_dir_prefix = "../img/time_lapse_"
 output_dir = output_dir_prefix + time.strftime("%Y%m%d-%H%M%S")
 # make directory if it doesn't exist
 os.makedirs(output_dir, exist_ok=True)
 
 # Set the time between photos in seconds
-time_between_photos = 10
+time_between_photos = 1
 # Set the number of photos to take
 number_of_photos = 100
 
@@ -28,11 +28,10 @@ number_of_photos = 100
 picam2.start()
 for i in range(number_of_photos):
     picam2.capture_file(output_dir + "/photo" + str(i).zfill(4) + ".jpg")
-    # write the image number and time to a csv file
+    # write the image number and time to nearest ms to a csv file
     with open(output_dir + "/photo_times.csv", "a") as f:
-        f.write(str(i) + "," + time.strftime("%Y%m%d-%H%M%S") + "\n")
+        f.write(str(i) + "," + time.strftime("%Y%m%d-%H%M%S") + "," + str(time.time()) + "\n")
     time.sleep(time_between_photos)
-
 
 
 
